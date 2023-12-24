828d89d2fd3d1d5bca67e061766ba235c3cb1890
Aleksandr Lukianov <alexander>
2023-12-24 23:56:07 +0300

    hw3. block2

---
+++ block2/scripts.bash
@@ -0,0 +1,17 @@
+# Общая часть
+docker-compose exec kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --topic itmo2023 --partitions 1 --replication-factor 1
+docker-compose exec kafka kafka-topics.sh --bootstrap-server kafka:9092 --create --topic itmo2023_result --partitions 1 --replication-factor 1
+python3 producer.py
+
+# Далее в зависимости от типа window
+
+# tumbling 
+docker-compose exec jobmanager ./bin/flink run -py /opt/pyflink/block2/tumbling.py -d
+# session
+docker-compose exec jobmanager ./bin/flink run -py /opt/pyflink/block2/session.py -d
+# sliding
+docker-compose exec jobmanager ./bin/flink run -py /opt/pyflink/block2/sliding.py -d
+
+# И consumer
+
+python3 consumer.py
--- consumer.py
+++ consumer.py
@@ -1,26 +1,17 @@
 from kafka import KafkaConsumer
 
 
-@backoff(tries=10,sleep=60)
-def message_handler(value)->None:
-    print(value)
-
-
 def create_consumer():
     print("Connecting to Kafka brokers")
-    consumer = KafkaConsumer("itmo2023",
-                             group_id='itmo_group1',
+    consumer = KafkaConsumer("itmo2023_result",
+                             group_id="itmo_processed_group",
                              bootstrap_servers='localhost:29092',
                              auto_offset_reset='earliest',
                              enable_auto_commit=True)
 
     for message in consumer:
-        # send to http get (rest api) to get response
-        # save to db message (kafka) + external
-        message_handler(message)
+        #save to DB
         print(message)
-
-
 
 
 if __name__ == '__main__':
